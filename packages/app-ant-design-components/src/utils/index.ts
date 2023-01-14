2e780eddacac9941285eda4dcf678ed73285843e
DPY <dupeiyi0912@gmail.com>
2023-01-14 23:10:16 +0800

    feat(时间格式的处理函数): 封装格式化时间的函数
    使用dayjs进行处理

--- packages/app-ant-design-components/src/utils/index.ts
+++ packages/app-ant-design-components/src/utils/index.ts
@@ -1,12 +1,21 @@
 import { getSafetyObjectValue } from './/getObject';
+import {
+  conversionMomentValue,
+  dateFormatterMap,
+} from './conversionMomentValue';
 import { runFunction } from './runFunction';
 import {
   DataFormatMapType,
   transformKeySubmitValue,
 } from './transformKeySubmitValue';
 
-export type { DataFormatMapType };
+export type { DataFormatMapType, dateFormatterMap };
 /**
  * 统一导出
  */
-export { runFunction, getSafetyObjectValue, transformKeySubmitValue };
+export {
+  runFunction,
+  getSafetyObjectValue,
+  transformKeySubmitValue,
+  conversionMomentValue,
+};
