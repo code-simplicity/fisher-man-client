65d4f57696943e46d2e250b9ec4afe203b56e06a
DPY <dupeiyi0912@gmail.com>
2023-01-14 20:48:43 +0800

    perf(transformKeySubmitValue): 统一进行导出
    便于使用

--- packages/app-ant-design-components/src/utils/index.ts
+++ packages/app-ant-design-components/src/utils/index.ts
@@ -1,7 +1,12 @@
 import { getSafetyObjectValue } from './/getObject';
 import { runFunction } from './runFunction';
+import {
+  DataFormatMapType,
+  transformKeySubmitValue,
+} from './transformKeySubmitValue';
 
+export type { DataFormatMapType };
 /**
  * 统一导出
  */
-export { runFunction, getSafetyObjectValue };
+export { runFunction, getSafetyObjectValue, transformKeySubmitValue };
